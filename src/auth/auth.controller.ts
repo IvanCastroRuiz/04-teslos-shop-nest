7689cf4db2ed892fc74e2e93801b7ce8db21dace
IvanCastroRuiz <icastror@hotmail.com>
2023-03-26 23:33:39 -0500

    Modulo Auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,34 +1,92 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Req, Headers, SetMetadata } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { IncomingHttpHeaders } from 'http';
+
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { RawHeaders, GetUser, Auth } from './decorators';
+import { RoleProtected } from './decorators/role-protected.decorator';
+
+import { CreateUserDto, LoginUserDto } from './dto';
+import { User } from './entities/user.entity';
+import { UserRoleGuard } from './guards/user-role.guard';
+import { ValidRoles } from './interfaces';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
+
+
+  @Post('register')
+  createUser(@Body() createUserDto: CreateUserDto ) {
+    return this.authService.create( createUserDto );
   }
 
-  @Get()
-  findAll() {
-    return this.authService.findAll();
+  @Post('login')
+  loginUser(@Body() loginUserDto: LoginUserDto ) {
+    return this.authService.login( loginUserDto );
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
+  @Get('check-status')
+  @Auth()
+  checkAuthStatus(
+    @GetUser() user: User
+  ) {
+    return this.authService.checkAuthStatus( user );
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
+
+  @Get('private')
+  @UseGuards( AuthGuard() )
+  testingPrivateRoute(
+    @Req() request: Express.Request,
+    @GetUser() user: User,
+    @GetUser('email') userEmail: string,
+    
+    @RawHeaders() rawHeaders: string[],
+    @Headers() headers: IncomingHttpHeaders,
+  ) {
+
+
+    return {
+      ok: true,
+      message: 'Hola Mundo Private',
+      user,
+      userEmail,
+      rawHeaders,
+      headers
+    }
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+
+  // @SetMetadata('roles', ['admin','super-user'])
+
+  @Get('private2')
+  @RoleProtected( ValidRoles.superUser, ValidRoles.admin )
+  @UseGuards( AuthGuard(), UserRoleGuard )
+  privateRoute2(
+    @GetUser() user: User
+  ) {
+
+    return {
+      ok: true,
+      user
+    }
   }
+
+
+  @Get('private3')
+  @Auth( ValidRoles.admin )
+  privateRoute3(
+    @GetUser() user: User
+  ) {
+
+    return {
+      ok: true,
+      user
+    }
+  }
+
+
+
 }
--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,15 +1,17 @@
 import { Controller, Get, } from '@nestjs/common';
-import { SeedService } from './seed.service';
 
+import { ValidRoles } from '../auth/interfaces';
+import { Auth } from '../auth/decorators';
+
+import { SeedService } from './seed.service';
 
 @Controller('seed')
 export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
-  
-
   @Get()
-  executesSeed(){
-     this.seedService.runSeed();
+  // @Auth( ValidRoles.admin )
+  executeSeed() {
+    return this.seedService.runSeed()
   }
 }
